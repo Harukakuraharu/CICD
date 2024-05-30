fec23f6ed576c1f6ed15e0b1b39f4dcb28bbf0f5
Gulnaz Artykaeva <145430688+Harukakuraharu@users.noreply.github.com>
2024-05-30 16:15:09 +0500

    Update views.py

--- students/views.py
+++ students/views.py
@@ -18,4 +18,4 @@ class CoursesViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Пока начало'})
+    return Response({'message': 'ПОЛУЧИЛОСЬ?????'})
