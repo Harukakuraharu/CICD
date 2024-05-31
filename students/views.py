234bc287758ed95376af37fd0083bcf617904645
Gulnaz Artykaeva <145430688+Harukakuraharu@users.noreply.github.com>
2024-05-31 15:00:53 +0500

    Update views.py

--- students/views.py
+++ students/views.py
@@ -18,4 +18,4 @@ class CoursesViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Работает?'})
+    return Response({'message': 'мехехеехехехехеех'})
